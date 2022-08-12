3a1c0272042b4266db915356ebc93f5601486c53
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-08-11 22:45:24 -0400

    CREATED VIEW AND CONTROLLER WITH A GET

--- src/main/java/com/mailingdelgadomedina/HomeController.java
+++ src/main/java/com/mailingdelgadomedina/HomeController.java
@@ -1,8 +1,17 @@
 package com.mailingdelgadomedina;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	//This is a Get request
+	
+	@GetMapping("/" )
+	public String getHomeView () {
+		return "index";
+		
+	}
+	
 }
