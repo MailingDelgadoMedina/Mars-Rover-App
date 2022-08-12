fc66c7fa7ea0d02b200bd8397a0701b2ee4d5252
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-08-11 23:23:56 -0400

    Added model with comments and set the cache to false

--- src/main/java/com/mailingdelgadomedina/HomeController.java
+++ src/main/java/com/mailingdelgadomedina/HomeController.java
@@ -1,6 +1,8 @@
 package com.mailingdelgadomedina;
 
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,7 +11,12 @@ public class HomeController {
 	//This is a Get request
 	
 	@GetMapping("/" )
-	public String getHomeView () {
+	public String getHomeView (ModelMap model) {
+		/*The parameters first had Model and on line 19 was .addAttribute the method
+		 * instead of put*/
+		/*Line 19 is the model that acts like a json 
+		 * object name being the key an my name the value*/
+		model.put("name", "Mailing Delgado");
 		return "index";
 		
 	}
